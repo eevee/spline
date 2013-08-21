30c0c695388f56b096e836c54d07b131c50138cb
Eevee (Alex Munroe) <eevee.git@veekun.com>
2013-08-20 21:49:05 -0700

    Shoehorn in multiple comic support.

--- splinter_comic/models.py
+++ splinter_comic/models.py
@@ -5,10 +5,28 @@ from sqlalchemy import (
     Unicode,
     UnicodeText,
 )
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import relationship
 
 from splinter.models import Base, User
 from splinter.models import Prose, TZDateTime, now
+
+
+class Comic(Base):
+    __tablename__ = 'comics'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    title = Column(Unicode, nullable=False)
+
+
+class ComicChapter(Base):
+    __tablename__ = 'comic_chapters'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    comic_id = Column(Integer, ForeignKey(Comic.id), nullable=False)
+    title = Column(Unicode, nullable=False)
+
+    comic = relationship(Comic, backref='chapters')
 
 
 class ComicPage(Base):
@@ -17,15 +35,11 @@ class ComicPage(Base):
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     timestamp = Column(TZDateTime, nullable=False, index=True, default=now)
     author_user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    chapter_id = Column(Integer, ForeignKey('comic_chapters.id'), nullable=True)
+    chapter_id = Column(Integer, ForeignKey(ComicChapter.id), nullable=True)
     file = Column(UnicodeText, nullable=False)
     title = Column(Unicode, nullable=False)
     comment = Column(Prose, nullable=False)
 
     author = relationship(User, backref='comic_pages')
-
-class ComicChapter(Base):
-    __tablename__ = 'comic_chapters'
-
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    title = Column(Unicode, nullable=False)
+    chapter = relationship(ComicChapter, backref='pages')
+    comic = association_proxy('chapter', 'comic')
